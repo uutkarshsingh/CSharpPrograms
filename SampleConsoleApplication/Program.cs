6c4fe13dad6ed2515717063a6d59a8c6db28328a
utkarsh rathor <utkarsh rathor>
2016-10-16 06:33:32 -0700

    Information about thr struct

--- SampleConsoleApplication/Program.cs
+++ SampleConsoleApplication/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace SampleConsoleApplication
 {
+
+    struct Dimension
+    {
+        public int Length;
+        public int Breadth;
+
+        public Dimension(int leangth ,int breadth)
+        {
+            System.Console.WriteLine("Constructin gthe obejct from the struct");
+            Length = leangth;
+            Breadth = breadth;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
+
+            Dimension dimension2;
+            dimension2.Length = 21;
+            dimension2.Breadth = 100;
+
+            System.Console.WriteLine(dimension2.Breadth);
+            Console.WriteLine(dimension2.Length);
+
+            System.ValueType atr ;
+
+            Dimension dimension1 = new Dimension();
+            int vaa = dimension1.Length;
+            Console.WriteLine("PRINTGINT SOMEVALUS FOR SHOWING ON THE SCREEN");
+            Console.ReadKey();
         }
     }
 }
